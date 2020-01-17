1b7164bb1489245d55a24497d820608ddeaee5fa
Anastasiia Vasileva <Anastasiia_Vasileva@epam.com>
2020-01-17 12:51:08 +0300

    moved Root component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { configureStore } from "./store/configureStore";
-import Root from "./components/Root";
+import Root from "./Root";
 
 const store = configureStore();
 ReactDOM.render(<Root store={store} />, document.getElementById("root"));
